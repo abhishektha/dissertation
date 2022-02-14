2168379ba5f0f404100357d6ee1cdd7ffd7671cb
Abhishektha Boppana <github@mail.abhishektha.com>
2022-02-13 23:58:37 -0700

    added main text about boot design

--- makePDF.sh
+++ makePDF.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 pandoc content/0*.md -o "./tex/Boppana_ComprehensiveExam.tex" --template "./tex/CU_Comps_Pandoc.tex" -F pandoc-crossref --top-level-division=chapter --natbib --wrap=preserve
 cd tex
-pdflatex --interaction=nonstopmode --output-directory=pdf Boppana_ComprehensiveExam.tex
-bibtex pdf/Boppana_ComprehensiveExam 
-pdflatex --interaction=nonstopmode --output-directory=pdf Boppana_ComprehensiveExam.tex
-pdflatex --interaction=nonstopmode --output-directory=pdf Boppana_ComprehensiveExam.tex
+latexdiff Boppana_ComprehensiveExam-Final.tex Boppana_ComprehensiveExam.tex > Boppana_Dissertation_Draft01.00.00.tex
+pdflatex --interaction=nonstopmode --output-directory=pdf Boppana_Dissertation_Draft01.00.00.tex
+bibtex pdf/Boppana_Dissertation_Draft01.00.00
+pdflatex --interaction=nonstopmode --output-directory=pdf Boppana_Dissertation_Draft01.00.00.tex
+pdflatex --interaction=nonstopmode --output-directory=pdf Boppana_Dissertation_Draft01.00.00.tex
 cd ..
